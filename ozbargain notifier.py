c64c56c035c94249ee3c81884ee9697334949ebb
Jack <jackzhuoyang@gmail.com>
2019-06-05 13:21:12 +1000

    Changed body text to link

--- ozbargain notifier.py
+++ ozbargain notifier.py
@@ -10,13 +10,12 @@ notify2.init("ozbargain notifier")
 def page_loader():
     url = requests.get("https://www.ozbargain.com.au/deals").text
     soup = BeautifulSoup(url, 'lxml')
-    page_loader.title = soup.find("h2", class_="title").text
-    page_loader.submitted = soup.find("div", class_="submitted").text
+    page_loader.title = soup.find("h2", class_="title")
+    page_loader.link = "https://www.ozbargain.com.au/" + page_loader.title.a["href"]
 
 def deal_notifier():
-    notification = notify2.Notification(page_loader.title,
-                            page_loader.submitted,
-                            "notification-message-im"   # Icon name
+    notification = notify2.Notification(page_loader.title.text,
+                            str(page_loader.link),
                             )
     notification.show()
 
@@ -24,10 +23,10 @@ last_deal = ""
 
 while True:
     page_loader()
-    if last_deal != page_loader.title:
+    if last_deal != page_loader.title.text:
         deal_notifier()
         print('"deal" found!')
     else:
         print("no new deals")
-    last_deal = page_loader.title
+    last_deal = page_loader.title.text
     time.sleep(20.0)
